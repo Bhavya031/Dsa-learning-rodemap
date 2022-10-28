5e093e54ef6357d15fef1b3f6bccaec309d86821
Bhavya031 <98141026+Bhavya031@users.noreply.github.com>
2022-10-28 16:07:39 +0530

    day1

---
+++ Day-1/Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your word:");
+        char word[]=input.next().toCharArray();
+        char reverse[] = new char[word.length];
+        int num=0;
+
+        for (num= 0;num<word.length;num++){
+            reverse[num]=word[word.length-num-1];
+        }
+        if (reverse==word){
+            System.out.println("this word is Palindrome word");
+        }
+        else {
+            System.out.println("this word is Palindrome word");
+        }
+    }
+}
---
+++ Day-1/sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("enter your fist number:");
+        int a= input.nextInt();
+        System.out.print("enter your second number:");
+        int b= input.nextInt();
+        int sum=a+b;
+        System.out.println("sum of two number is:"+sum);
+    }
+
+}
