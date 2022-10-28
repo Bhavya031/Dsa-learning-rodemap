ebce0ada2b6621917ca2e9d2ee58d817b329ca10
Bhavya031 <98141026+Bhavya031@users.noreply.github.com>
2022-10-28 16:16:39 +0530

    Day-1

--- Day-1/largestnumber.java
+++ Day-1/largestnumber.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class largestnumber {
-    public static void main(St ring[] args) {
+    public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("enter the first number:");
         int num1=input.nextInt();
