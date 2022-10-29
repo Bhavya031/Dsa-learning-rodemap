a2e4affa760d450ee91a7cbfc08c86e485b26c37
Bhavya031 <98141026+Bhavya031@users.noreply.github.com>
2022-10-29 07:01:47 +0530

    Day-1

--- Day-1/calculator.java
+++ Day-1/calculator.java
@@ -3,29 +3,26 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter your first number:");
-        int num1 = input.nextInt();
-        System.out.print("Enter your second number:");
-        int num2 = input.nextInt();
-        System.out.print("Enter your operator :");
-        var operator = input.next();
-        int calculate=0;
-        if (operator.equals("+")) {
-            calculate = num1 + num2;
+        while (true) {
+            System.out.print("Enter your first number:");
+            int num1 = input.nextInt();
+            System.out.print("Enter your second number:");
+            int num2 = input.nextInt();
+            System.out.print("Enter your operator:");
+            var operator = input.next();
+            int calculate = 0;
+            if (operator.equals("+")) {
+                calculate = num1 + num2;
+            } else if (operator.equals("-")) {
+                calculate = num1 - num2;
+            } else if (operator.equals("*")) {
+                calculate = num1 * num2;
+            } else if (operator.equals("/")) {
+                calculate = num1 / num2;
+            } else {
+                System.out.println("Error");
+            }
+            System.out.println("Total:"+calculate);
         }
-        else if (operator.equals("-")) {
-            calculate = num1 - num2;
-        }
-        else if (operator.equals("*")) {
-            calculate = num1 * num2;
-        }
-        else if (operator.equals("/")) {
-            calculate = num1 / num2;
-        }
-        else
-        {
-            System.out.println("Error");
-        }
-        System.out.println(calculate);
     }
 }
