8d4ebdb6467cbff53b03b723f2f20fd0f58e6bfe
Bhavya031 <98141026+Bhavya031@users.noreply.github.com>
2022-10-29 06:14:18 +0530

    Day-1

--- Day-1/Fibonacci.java
+++ Day-1/Fibonacci.java
@@ -4,12 +4,17 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.print("enter your number:");
-        int num1=input.nextInt();
-        int num3=num1;
-        int num2;
-        for (num2=1;num2<num3;num2++){
-            num1=num1+num2;
+        int n = input.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        while (count <= n){
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
         }
-        System.out.print("Fibonacci Series:"+num1);
+        System.out.println(b);
+
     }
 }
