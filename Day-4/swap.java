7a8ee682c782e3cabdfda109df2b9eb2a914f0a4
Bhavya031 <98141026+Bhavya031@users.noreply.github.com>
2022-11-01 15:09:24 +0530

    Day-4

---
+++ Day-4/swap.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class swap {
+    public static void main(String[] args) {
+        int[] a={0,1,2,3,4,5,6,7,8,9,10};
+        swap1(a);
+        System.out.println(Arrays.toString(a));
+    }
+    static void swap1(int[] numb){
+            numb[0]=212121;
+    }
+}
