85cff4ef8bba4e4e73570caa1ad439d0bf7fdcfc
Bhavya031 <98141026+Bhavya031@users.noreply.github.com>
2022-11-04 14:06:47 +0530

    Day-5

--- Day-6/Array.java
+++ Day-6/Array.java
@@ -1,2 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Array {
+    Scanner input=new Scanner(System.in);
+    public static void main(String[] args) {
+        strarr();
+        intarr();
+    }
+    static void strarr(){
+        System.out.println("Enter any Five string");
+        String[] a =new String[5];
+        Scanner input=new Scanner(System.in);
+        for(int i=0;i< a.length;i++) {
+            a[i] = input.nextLine();
+            System.out.println(Arrays.toString(a));
+        }
+
+    }
+    static void intarr(){
+        System.out.print("enter any five number:");
+        Scanner input=new Scanner(System.in);
+        int[] a =new int[5];
+        for(int i=0;i< a.length;i++){
+            a[i]=input.nextInt();
+            System.out.println(Arrays.toString(a));
+        }
+    }
 }
