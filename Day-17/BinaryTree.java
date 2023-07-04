d251ddb1446be064aba2945e8158a1d23995e750
Bhavya Patel <2021003493.adit@cvmu.edu.in>
2023-07-04 22:34:17 +0530

    BinaryTree changes

--- Day-17/BinaryTree.java
+++ Day-17/BinaryTree.java
@@ -1,15 +1,66 @@
-import java.lang.reflect.Constructor;
-
 public class BinaryTree {
-//binary tree node class containing data, left and right child
-static class Node {
-int data;
-Node left, right;
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
+
+    private Node root;
+
+    private class Node {
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
     }
-}
-//constructor for binary tree node
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public void insert(int data) {
+        root = insertNode(root, data);
+    }
+
+    private Node insertNode(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = insertNode(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertNode(root.right, data);
+        }
+
+        return root;
+    }
+
+    public void inorderTraversal() {
+        inorder(root);
+    }
+
+    private void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        System.out.println("Inorder traversal of the binary tree:");
+        tree.inorderTraversal();
+    }
 }
